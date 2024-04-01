999d86905be357c685c3d312b7be4994f458fd5b
huzihao2003 <huzihao@enwaynet.com>
2024-04-01 10:03:45 +0800

    导出中文乱码

--- src/views/ConterView.vue
+++ src/views/ConterView.vue
@@ -127,7 +127,7 @@ export default {
             this.saveCSV(csv, "data.csv");
         },
         saveCSV(csv, fileName) {
-            const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+            const blob = new Blob(['\uFEFF'+csv], { type: "text/csv;charset=utf-8;" });
             const url = URL.createObjectURL(blob);
             const link = document.createElement("a");
             link.setAttribute("href", url);
